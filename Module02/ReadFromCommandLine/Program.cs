3e93a925d83960760aa451ffb1318549139cd1ca
mo3aky <nikmirchev@gmail.com>
2016-08-22 21:10:22 +0300

    Модул 2, Видео 5 - Четене от командния ред

---
+++ Module02/ReadFromCommandLine/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReadFromCommandLine
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			int a = 10, b = 0;
+
+			Console.WriteLine ("Моля Въведете а: ");
+			b = Convert.ToInt32 (Console.ReadLine ());
+		
+			Console.WriteLine ( "Резултатът a + b e " + (a + b).ToString () + "\n\n\n");
+		}
+	}
+}
