170576c130f0fe38313b5c278e365e8c42fd2182
mo3aky <nikmirchev@gmail.com>
2016-08-30 21:38:37 +0300

    Работа със стрингове

--- Module02/ReadFromCommandLine/Program.cs
+++ Module02/ReadFromCommandLine/Program.cs
@@ -13,6 +13,19 @@ namespace ReadFromCommandLine
 			b = Convert.ToInt32 (Console.ReadLine ());
 		
 			Console.WriteLine ( "Резултатът a + b e " + (a + b).ToString () + "\n\n\n");
+
+			string test= "1";
+			test += ",2";
+			test += ",100";
+			test += ",4";
+			test += ",5";
+
+			Console.Write("Събиране с += " + test + "\n\n");
+
+			test = test.Replace ( ",", ";");
+			Console.WriteLine ("Работа с Replace " + test + "\n\n");
+
+			Console.WriteLine ("3-ти знак " + test.Split(';')[2] + "\n");
 		}
 	}
 }
